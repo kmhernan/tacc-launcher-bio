d2e98ce9c20bb71626eb7b95aeb4a138878b1e08
Kyle Hernandez <kmhernan@utexas.edu>
2013-08-26 17:34:58 -0500

    Update first-script.sh

--- sample-bash-scripts/first-script.sh
+++ sample-bash-scripts/first-script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Kyle Hernandez
-# first-script.sh - An example script, showcasing the various components of a bash script
+# Aug 26 2013
+# simple-bash-1.sh - Prints the files in the current directory to the console
 
- 
+for fil in ./*; do
+    BASE=$(basename $fil)
+    PREFIX=${BASE%.*}
+    SUFFIX=${BASE#*.}
+    echo "NAME=$fil - BASE=$BASE - PREFIX=$PREFIX - SUFFIX=$SUFFIX"
+done
